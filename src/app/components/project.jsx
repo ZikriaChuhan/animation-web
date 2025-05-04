5bbfafd1791871468616c6e7fd9b5b9bd080d290
ZikriaChuhan <chuhanzikria@gmail.com>
2025-05-05 02:06:19 +0500

    first commit

--- src/app/components/project.jsx
+++ src/app/components/project.jsx
@@ -202,6 +202,7 @@ export default function Project() {
                         </span>
                     </button>
                 </div>
+                
             </section>
         </>
     );
